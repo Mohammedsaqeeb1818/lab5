f098ac208107a1d964e4f08de3b381cc748636f6
Mohammedsaqeeb1818 <mdsaqeeb081104@gmail.com>
2024-09-30 15:35:33 +0530

    Create demo.java

---
+++ demo.java
@@ -0,0 +1,14 @@
+import java.io.*
+  class demo
+  {
+    public static void main(String args[])
+    {
+      int a,b,sum;
+      Scanner in=new Scanner(System.in);
+      System.out.println("Enter the two number a and b");
+      a=in.nextInt();
+      b=in.nextInt();
+      System.out.println("The sum if two numbers is:"+(a+b));
+    }
+  }
+    
